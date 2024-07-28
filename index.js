df7633fab7fd2744261d753077f04884a431bb1e
190101101 <190101101@mail.ru>
2024-07-28 05:14:56 +0400

    1.2

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-fetch('https://api.hamsterkombatgame.io/clicker/tap', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization:
-      'Bearer 1721509825647mvnIWoHCAjZ30DUdoPRCsREA4952IbdjeWwmZNhmdxshd2mgL3wnn0X3uoCpG0Ts1572965406',
-  },
-  body: JSON.stringify({
-    count: 1000000,
-    availableTaps: 1000000,
-    timestamp: new Date().getTime()
-  }),
-})
-  .then((response) => {
+async function fetcher() {
+  try {
+    const response = await fetch('https://api.hamsterkombatgame.io/clicker/tap', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer 1721509825647mvnIWoHCAjZ30DUdoPRCsREA4952IbdjeWwmZNhmdxshd2mgL3wnn0X3uoCpG0Ts1572965406',
+      },
+      body: JSON.stringify({
+        count: 1000000,
+        availableTaps: 1000000,
+        timestamp: new Date().getTime(),
+      }),
+    });
+
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
     }
-    return response.json();
-  })
-  .then((data) => {
-    console.log('Success:', data);
-  })
-  .catch((error) => {
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
     console.error('Error:', error);
+    return null; 
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const balanceCoinBlockContext = document.querySelector('#balanceCoinBlockContext');
+  const balanceCoinSpanContext = document.createElement('span');
+  balanceCoinBlockContext.appendChild(balanceCoinSpanContext);
+
+  fetcher().then(data => {
+    if (data) {
+      console.log(data);
+      balanceCoinSpanContext.innerHTML = data.clickerUser.balanceCoins;
+    } else {
+      balanceCoinSpanContext.innerHTML = 'No data returned due to an error';
+    }
   });
+});
+
